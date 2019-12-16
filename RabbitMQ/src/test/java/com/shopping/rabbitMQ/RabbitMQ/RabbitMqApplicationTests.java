d25e50f3cdfa9f07a5d6a49b6e45642022ff1480
Pin2Shaw <36593803+Pin2Shaw@users.noreply.github.com>
2019-12-16 05:29:55 -0800

    Add files via upload
    Rabbit MQ

---
+++ RabbitMQ/src/test/java/com/shopping/rabbitMQ/RabbitMQ/RabbitMqApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shopping.rabbitMQ.RabbitMQ;
+
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
